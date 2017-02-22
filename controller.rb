618fb50b8f99614baf9753dd374cb3790cc5b086
David Hale <dhale12@hotmail.co.uk>
2017-02-22 14:03:05 +0000

    add first view

---
+++ controller.rb
@@ -0,0 +1,8 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('./models/student.rb')
+
+get '/students' do
+  @students = Student.all()
+  erb(:index)
+end
--- models/student.rb
+++ models/student.rb
@@ -1,6 +1,8 @@
 require_relative('../db/sql_runner.rb')
 
 class Student
+  attr_reader :id, :first_name, :last_name, :house, :age 
+
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @first_name = options['first_name']
